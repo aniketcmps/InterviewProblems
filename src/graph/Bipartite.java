3f068f3bc2e2dd99c33d9c1f89637a3941a4858c
aadeshp4 <aadeshp4@EN4119365214X.fulton.ad.asu.edu>
2017-02-09 15:37:06 -0700

    adding comments

--- src/graph/Bipartite.java
+++ src/graph/Bipartite.java
@@ -3,6 +3,12 @@ package graph;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+/**
+ * Check whether a given graph is Bipartite or not A Bipartite Graph is a graph
+ * whose vertices can be divided into two independent sets, U and V such that
+ * every edge (u, v) either connects a vertex from U to V or a vertex from V to
+ * U. O(V+E)
+ */
 class Bipartite {
 	public boolean isbipartite(Graph g, int s) {
 		LinkedList<Integer> queue = new LinkedList<Integer>();
@@ -21,9 +27,7 @@ class Bipartite {
 			while (i.hasNext()) {
 				int x = i.next();
 				if (color[x] == c) {
-					if (color[n] == c) {
-						return false;
-					}
+					return false; // next node with same color as current 
 				} else if (color[x] == -1) {
 					color[x] = c;
 					queue.add(x);
@@ -45,7 +49,7 @@ class Bipartite {
 		g.addEdge(2, 3);
 		g.addEdge(3, 4);
 		g.addEdge(4, 5);
-		g.addEdge(1, 0);
+		g.addEdge(3, 0);
 
 		Bipartite b = new Bipartite();
 		if (b.isbipartite(g, 0)) {
